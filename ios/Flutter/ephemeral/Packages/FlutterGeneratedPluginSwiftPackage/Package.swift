2b015654d4e6275a06294b92e490a95dc5b37d45
mamadou ndiaye <modzon4n@gmail.com>
2026-07-18 10:57:28 +0000

    Use static linkage in Podfile and remove duplicate SPM packages

--- ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -9,50 +9,17 @@ import PackageDescription
 let package = Package(
     name: "FlutterGeneratedPluginSwiftPackage",
     platforms: [
-        .iOS("15.0")
+        .iOS("13.0")
     ],
     products: [
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        .package(name: "audioplayers_darwin", path: "../.packages/audioplayers_darwin-6.3.0"),
-        .package(name: "connectivity_plus", path: "../.packages/connectivity_plus-7.0.0"),
-        .package(name: "file_picker", path: "../.packages/file_picker-10.3.3"),
-        .package(name: "firebase_core", path: "../.packages/firebase_core-4.1.1"),
-        .package(name: "firebase_messaging", path: "../.packages/firebase_messaging-16.0.2"),
-        .package(name: "flutter_local_notifications", path: "../.packages/flutter_local_notifications-19.4.2"),
-        .package(name: "geolocator_apple", path: "../.packages/geolocator_apple-2.3.13"),
-        .package(name: "image_picker_ios", path: "../.packages/image_picker_ios-0.8.13"),
-        .package(name: "path_provider_foundation", path: "../.packages/path_provider_foundation-2.4.0"),
-        .package(name: "pointer_interceptor_ios", path: "../.packages/pointer_interceptor_ios-0.10.1"),
-        .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.3"),
-        .package(name: "sqflite_darwin", path: "../.packages/sqflite_darwin-2.4.1-1"),
-        .package(name: "url_launcher_ios", path: "../.packages/url_launcher_ios-6.3.1"),
-        .package(name: "video_player_avfoundation", path: "../.packages/video_player_avfoundation-2.8.4"),
-        .package(name: "wakelock_plus", path: "../.packages/wakelock_plus-1.3.2"),
-        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
+        
     ],
     targets: [
         .target(
-            name: "FlutterGeneratedPluginSwiftPackage",
-            dependencies: [
-                .product(name: "audioplayers-darwin", package: "audioplayers_darwin"),
-                .product(name: "connectivity-plus", package: "connectivity_plus"),
-                .product(name: "file-picker", package: "file_picker"),
-                .product(name: "firebase-core", package: "firebase_core"),
-                .product(name: "firebase-messaging", package: "firebase_messaging"),
-                .product(name: "flutter-local-notifications", package: "flutter_local_notifications"),
-                .product(name: "geolocator-apple", package: "geolocator_apple"),
-                .product(name: "image-picker-ios", package: "image_picker_ios"),
-                .product(name: "path-provider-foundation", package: "path_provider_foundation"),
-                .product(name: "pointer-interceptor-ios", package: "pointer_interceptor_ios"),
-                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
-                .product(name: "sqflite-darwin", package: "sqflite_darwin"),
-                .product(name: "url-launcher-ios", package: "url_launcher_ios"),
-                .product(name: "video-player-avfoundation", package: "video_player_avfoundation"),
-                .product(name: "wakelock-plus", package: "wakelock_plus"),
-                .product(name: "FlutterFramework", package: "FlutterFramework")
-            ]
+            name: "FlutterGeneratedPluginSwiftPackage"
         )
     ]
 )
